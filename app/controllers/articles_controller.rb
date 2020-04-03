fad74af5435ba1c4b84888634eb37e6e1c963604
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2020-04-03 09:55:59 +0300

    Save new article to db and show it on the view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,23 @@
 class ArticlesController < ApplicationController
+  # Best practice for methods order
+  # index, show, new, edit, create, update, destroy
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def new
   end
 
   def create
-    render plain: params[:article].inspect
+    @article = Article.new(article_params)
+
+    @article.save
+    redirect_to @article
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :text)
   end
 end
